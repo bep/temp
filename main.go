3dd0aa85fd70ffa837fb57943eac7cd5cecd365a
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2017-05-02 10:48:46 +0200

    Hugo godocs

--- main.go
+++ main.go
@@ -2,20 +2,57 @@ package main
 
 import (
 	"fmt"
-	"reflect"
-	"runtime"
+	"go/doc"
+	"go/parser"
+	"go/token"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+	"strings"
 )
 
-type A struct {
-}
-
-func (a *A) M() {
-
-}
-
+// Main docs
 func main() {
-	a := &A{}
+	fset := token.NewFileSet()
+	basePath := "/Users/bep/go/src/github.com/spf13/hugo/tpl"
 
-	name := runtime.FuncForPC(reflect.ValueOf(a.M).Pointer()).Name()
-	fmt.Println("Name:", name)
+	files, err := ioutil.ReadDir(basePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, fi := range files {
+		if !fi.IsDir() {
+			continue
+		}
+
+		packagePath := filepath.Join(basePath, fi.Name())
+
+		d, err := parser.ParseDir(fset, packagePath, nil, parser.ParseComments)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, f := range d {
+			p := doc.New(f, "./", 0)
+
+			for _, t := range p.Types {
+				if t.Name == "Namespace" {
+					fmt.Println("  type", t.Name)
+					fmt.Println("    docs:", t.Doc)
+					for _, tt := range t.Methods {
+						fmt.Println("       ", tt.Name)
+						fmt.Println("          ", tt.Doc)
+						var params []string
+						for _, p := range tt.Decl.Type.Params.List {
+							for _, pp := range p.Names {
+								params = append(params, pp.Name)
+							}
+						}
+						fmt.Println("          ", strings.Join(params, ", "))
+					}
+				}
+			}
+		}
+	}
 }
