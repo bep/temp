ea074f5869ec220edc613ff54f882310b1ae5173
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2017-07-30 21:31:50 +0200

    Add repro for jdkato/prose
    See https://github.com/jdkato/prose/issues/14

--- main.go
+++ main.go
@@ -1,36 +1,8 @@
 package main
 
-import "fmt"
-
-func main() {
-
-	queue1 := make(chan string, 2)
-	queue1 <- "one"
-	queue1 <- "two"
-	close(queue1)
-
-	for elem := range queue1 {
-		fmt.Println(elem)
+type (
+	string string
+	a      struct {
+		m map[string]int
 	}
-
-	fmt.Println("Done...")
-
-	queue2 := make(chan string, 2)
-	queue2 <- "one"
-	queue2 <- "two"
-	close(queue2)
-
-LOOP:
-	for {
-		select {
-		case elem, ok := <-queue2:
-			if !ok {
-				break LOOP
-			}
-			fmt.Println(elem)
-		}
-	}
-
-	fmt.Println("Done...")
-
-}
+)
