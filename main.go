df9ea8bda5eb5a7e9197c80706037db43f877d39
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2017-09-01 08:55:04 +0200

    map.Sync LoadOrStore for caching

--- main.go
+++ main.go
@@ -1,80 +1,36 @@
 package main
 
 import (
-	"context"
-	"flag"
 	"fmt"
-	"log"
-	"net"
-	"net/http"
-	"net/url"
-	"strings"
-	"time"
+	"sync"
 )
+
+type cache struct {
+	mm sync.Map
+}
+
+func (c *cache) get(key interface{}, fn func() interface{}) interface{} {
+	if v, ok := c.mm.Load(key); ok {
+		return v
+	}
+
+	if v, loaded := c.mm.LoadOrStore(key, fn()); loaded {
+		return v
+	} else {
+		return v
+	}
+
+}
 
 func main() {
 
-	var (
-		signedURL string
-		method    string
-		help      bool
-	)
-
-	flag.StringVar(&signedURL, "url", "", "Signed URL")
-	flag.StringVar(&method, "method", "", "HTTP method")
-	flag.BoolVar(&help, "h", false, "help")
-
-	flag.Parse()
-
-	if help {
-		flag.Usage()
-		return
+	fn := func() interface{} {
+		fmt.Println("fn executed")
+		return "val"
 	}
 
-	URL, err := url.Parse(signedURL)
-	if err != nil {
-		log.Fatal(err)
-	}
+	c := &cache{}
 
-	host := strings.Split(URL.Host, ":")[0]
-
-	nodes, err := net.LookupHost(host)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	dialer := &net.Dialer{
-		Timeout:   30 * time.Second,
-		KeepAlive: 30 * time.Second,
-		DualStack: true,
-	}
-
-	for _, node := range nodes {
-		// This could be less hacky with https://github.com/golang/go/issues/12503
-		transport := &http.Transport{
-			DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
-				addr = node + ":" + strings.Split(addr, ":")[1]
-				return dialer.DialContext(ctx, network, addr)
-			},
-		}
-
-		c := &http.Client{Timeout: 10 * time.Second, Transport: transport}
-
-		req, err := http.NewRequest(method, signedURL, nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		resp, err := c.Do(req)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("HTTP status:", resp.StatusCode)
-		for k, v := range resp.Header {
-			fmt.Println(k, "=>", v)
-		}
-
-		resp.Body.Close()
-	}
-
+	fmt.Println(c.get("key", fn))
+	fmt.Println(c.get("key", fn))
 }
