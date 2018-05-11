dc510aa1a4a019bea3ff85c059e727448e05ff76
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2018-05-11 16:25:31 +0200

    Lazy

--- main.go
+++ main.go
@@ -1,11 +1,45 @@
 package main
 
 import (
-	"github.com/bep/temp/foo"
+	"bytes"
+	"fmt"
+	"text/template"
 )
+
+type MM map[string]interface{}
+
+func (m MM) MyMy() string {
+	return fmt.Sprintf("ABC: %v\n", m["a"])
+}
+
+type A struct {
+	Func func() []string
+	Map  map[string]interface{}
+}
+
+func (A) Method() []string {
+	return []string{"c", "d"}
+}
 
 func main() {
 
-	foo.Run()
+	m := make(MM)
+
+	m["a"] = "aa"
+	m["b"] = "bb"
+
+	for _, tpl := range []string{"{{ range $k, $v := .  }}-- {{ $k }}: {{ $v }}{{ end }} => {{ .MyMy }}"} {
+
+		var buf bytes.Buffer
+		tmpl, err := template.New("").Parse(tpl)
+		if err != nil {
+			panic(err)
+		}
+		if err := tmpl.Execute(&buf, m); err != nil {
+			panic(err)
+		}
+
+		fmt.Println(buf.String())
+	}
 
 }
