2ce6de6068659d0ff8ef7c9bad231e940f5336ac
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2018-08-23 19:54:48 +0200

    Go template bug

--- main.go
+++ main.go
@@ -1,57 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"html/template"
 	"log"
-
-	"reflect"
-
-	"github.com/mitchellh/mapstructure"
+	"strings"
 )
 
-type T struct {
-	M1 map[string]interface{}
-	S1 []interface{}
-	S2 []interface{}
-}
-
 func main() {
-	dest := &T{}
-
-	hook := func(t1 reflect.Type, t2 reflect.Type, v interface{}) (interface{}, error) {
-		fmt.Printf("%v\n", v)
-		return v, nil
+	data := map[string]interface{}{
+		"style": template.HTMLAttr("background-image: url(../images/logo.png)"),
 	}
+	tpl := `
+<section style="{{ .style }}">
+</section>
+`
 
-	input := map[string]interface{}{
-		"M1": map[string]interface{}{
-			"V1": "v1",
-			"M2": map[string]interface{}{
-				"V2": "v2",
-			},
-		},
-		"S1": []interface{}{"VS1_1", "VS1_2"},
-		"S2": []interface{}{map[string]interface{}{
-			"VMS": "vms",
-		}},
-	}
-
-	conf := &mapstructure.DecoderConfig{
-		DecodeHook:       hook,
-		ErrorUnused:      false,
-		WeaklyTypedInput: false,
-		Metadata:         nil,
-		Result:           dest,
-		ZeroFields:       true,
-	}
-
-	dec, err := mapstructure.NewDecoder(conf)
+	var buf bytes.Buffer
+	tmpl, err := template.New("").Parse(tpl)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = dec.Decode(input)
-	if err != nil {
+
+	if err := tmpl.Execute(&buf, data); err != nil {
 		log.Fatal(err)
+	}
+
+	result := strings.TrimSpace(buf.String())
+
+	if !strings.Contains(result, "background-image") {
+		log.Fatal(result)
 	}
 
 }
