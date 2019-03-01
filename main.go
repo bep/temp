45ab144a5ba8722d824047d491e968d7c07be38f
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2019-03-01 10:55:03 +0100

    Niller

--- main.go
+++ main.go
@@ -2,40 +2,52 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"log"
-	"strings"
 	"text/template"
 )
 
-type F struct {
-	name string
+type Niller interface {
+	Nil() Niller
+	Foo() string
 }
 
-func (f *F) Name() string {
-	return f.name
+type Nill struct {
+	nilv Niller
 }
 
-func (f *F) Other() *F {
-	return nil
+func (n Nill) Nil() Niller {
+	return n.nilv
+}
+
+func (n Nill) Foo() string {
+	return "asdf"
 }
 
 func main() {
-	var buf bytes.Buffer
-	tmpl, err := template.New("").Parse(`{{ .Other.Name }}`)
+
+	tmpl, err := template.New("").Parse(`{{ with .Nil }}Failed, got {{ . }}{{ else }}OK{{ end }}`)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	data := &F{name: "foo"}
+	var (
+		nil1 Niller = (*Nill)(nil)
+		nil2 Niller
+		nil3 *Nill
+		nil4 Niller = nil
+	)
 
-	if err := tmpl.Execute(&buf, data); err != nil {
-		log.Fatal(err)
+	for i, niller := range []Niller{nil1, nil2, nil3, nil4} {
+
+		var buff bytes.Buffer
+		n := &Nill{nilv: niller}
+		a, b := template.IsTrue(n.Nil())
+		err = tmpl.Execute(&buff, n)
+		if err != nil {
+			fmt.Println("error:", err)
+			continue
+		}
+		fmt.Println(i+1, a, b, buff.String())
 	}
-
-	result := strings.TrimSpace(buf.String())
-
-	if !strings.Contains(result, "foo") {
-		log.Fatal(result)
-	}
-
 }
