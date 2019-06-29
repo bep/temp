ffc5a79102ae541b25c8a45b2fb5dd2213ab5e7b
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2019-06-29 11:16:46 +0200

    Symlink

--- main.go
+++ main.go
@@ -1,136 +1,45 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"html/template"
+	"io/ioutil"
 	"log"
-	"math/rand"
-	"strings"
-	"sync"
-	"time"
+	"os"
+	"path/filepath"
 )
 
-// https://github.com/golang/go/blob/master/src/text/template/exec.go#L402
-// https://github.com/golang/go/blob/master/src/text/template/template.go#L222
-
 func main() {
-	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
-
-	var clonesMu sync.Mutex
-	clones := make(map[string]*template.Template)
-
-	var wg sync.WaitGroup
-
-	words := make([]string, 22)
-	for i := 0; i < 22; i++ {
-		words[i] = fmt.Sprintf("<word%d>", i)
+	tempDir, err := ioutil.TempDir("", "test-symlinks")
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer os.RemoveAll(tempDir)
 
-	scripts := make([]string, 22)
-	for i := 0; i < 22; i++ {
-		scripts[i] = js
-	}
+	wd, _ := os.Getwd()
+	defer func() {
+		os.Chdir(wd)
+	}()
 
-	data := map[string]interface{}{
-		"Words":   words,
-		"Scripts": scripts,
-	}
-
-	templateTemplate := `
-<h1>This is a template</h1>
-{{ range .Words }}
-<a href="{{ . }}">
-{{ end }}
-{{ range .Scripts }}
-{{ . }}
-{{ end }}
-{{ template "inline" . }}
-{{ define "inline" }}
-{{ $first := index .Words 0 }}
-<a href="{{ $first }}">
-{{ end }}
-`
-
-	tpl := template.New("")
-	for i := 0; i < 100; i++ {
-		_, err := tpl.New(fmt.Sprintf("innert%d", i)).Parse(templateTemplate)
+	must := func(err error) {
 		if err != nil {
-			log.Fatal(err)
+			panic(err)
 		}
 	}
 
-	for i := 0; i < 20; i++ {
-		var templ strings.Builder
-		for j := 0; j < 60; j++ {
-			templ.WriteString(fmt.Sprintf(`{{ template "innert%d" . }}`, rnd.Intn(100)))
-		}
-		templ.WriteString(templateTemplate)
-		name := fmt.Sprintf("outert%d", i)
-		_, err := tpl.New(name).Parse(templ.String())
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
+	dir1Dir := filepath.Join(tempDir, "dir1")
+	dir1Real := filepath.Join(dir1Dir, "real")
+	dir2Dir := filepath.Join(tempDir, "dir2")
 
-	for i := 0; i < 20; i++ {
-		name := fmt.Sprintf("outert%d", i)
-		t := tpl.Lookup(name)
-		if t == nil {
-			log.Fatal("not found")
-		}
-		tc, err := t.Clone()
-		if err != nil {
-			log.Fatal(err)
-		}
-		clones[name] = tc
+	must(os.MkdirAll(filepath.Join(dir1Real), 0777))
+	must(os.MkdirAll(filepath.Join(dir2Dir), 0777))
 
-	}
+	must(os.Chdir(dir1Dir))
+	must(os.Symlink("real", "symlinked"))
+	must(os.Chdir(dir1Real))
+	must(os.Symlink("../real", "cyclic"))
+	os.Chdir(dir2Dir)
+	must(os.Symlink("../dir1/real/cyclic", "dir2real"))
 
-	for i := 0; i < 20; i++ {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			for j := 0; j < 10; j++ {
-				name := fmt.Sprintf("outert%d", rand.Intn(20))
-				var templ *template.Template
-				r := rand.Intn(10)
-				if r < 5 {
-					clonesMu.Lock()
-					templ = clones[name]
-					clonesMu.Unlock()
-				} else if r < 7 {
-					templates := tpl.Templates()
-					templ = templates[rnd.Intn(len(templates))]
-				} else {
-					templ = tpl.Lookup(name)
-				}
+	_, err = filepath.EvalSymlinks(filepath.Join(dir2Dir, "dir2real", "cyclic"))
+	must(err)
 
-				if templ == nil {
-					log.Fatalf("%q not found", name)
-				}
-				var buf bytes.Buffer
-				if err := templ.Execute(&buf, data); err != nil {
-					log.Fatal(err)
-				}
-				if buf.Len() == 0 {
-					log.Fatal("nothing produced")
-				}
-			}
-		}(i)
-	}
-
-	wg.Wait()
-
-	fmt.Println("Done...")
 }
-
-const js = `
-<!-- Google Tag Manager -->
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-XXXX');
-<!-- End Google Tag Manager -->
-`
