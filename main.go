96c5cf1e5276cac5731674b6f18ad232d12fed6f
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2019-09-07 17:27:53 +0200

    Temp

--- main.go
+++ main.go
@@ -1,50 +1,101 @@
 package main
 
 import (
-	"log"
-	"math/big"
-	"time"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 
-	"github.com/bep/typedmapcodec"
-
-	"github.com/kr/pretty"
+	"github.com/starlight-go/starlight"
+	"github.com/starlight-go/starlight/convert"
+	"go.starlark.net/starlark"
 )
 
+type contact struct {
+	Name string
+}
+
+// https://jsonplaceholder.typicode.com/todos
+
 func main() {
-	mi := map[string]interface{}{
-		"vstring":   "Hello",
-		"vint":      32,
-		"vrat":      big.NewRat(1, 2),
-		"vtime":     time.Now(),
-		"vduration": 3 * time.Second,
-		"vsliceint": []int{1, 3, 4},
-		"nested": map[string]interface{}{
-			"vint":      55,
-			"vduration": 5 * time.Second,
-		},
-		"nested-typed-int": map[string]int{
-			"vint": 42,
-		},
-		"nested-typed-duration": map[string]time.Duration{
-			"v1": 5 * time.Second,
-			"v2": 10 * time.Second,
-		},
+	get := func(url string) io.Reader {
+		resp, err := http.Get(url)
+		if err != nil {
+			panic(err)
+		}
+		return resp.Body
 	}
 
-	c, err := typedmapcodec.New()
+	globals := map[string]interface{}{
+		"Get":     get,
+		"Println": fmt.Println,
+	}
+
+	script1 := []byte(`
+	
+def idiv(x, y):
+  return x // y
+
+
+def getTodos():
+	return Get("https://jsonplaceholder.typicode.com/todos")
+
+
+
+`)
+
+	out, err := starlight.Eval(script1, globals, nil)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	data, err := c.Marshal(mi)
+	f1 := out["getTodos"].(*starlark.Function)
+
+	runFunc(f1)
+
+}
+
+func decodeTODOs(r io.Reader) {
+	dec := json.NewDecoder(r)
+	var todos []interface{}
+	for {
+		var m interface{}
+		if err := dec.Decode(&m); err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+
+		todos = append(todos, m)
+	}
+
+	fmt.Println("TODOS:", todos)
+}
+
+func runFunc(fn *starlark.Function) {
+	thread := &starlark.Thread{}
+	v, err := starlark.Call(thread, fn, nil, nil)
 	if err != nil {
-		log.Fatal(err)
-	}
-	m := make(map[string]interface{})
-	if err := c.Unmarshal(data, &m); err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	pretty.Print(m)
+	rrc := convert.FromValue(v).(io.ReadCloser)
 
+	decodeTODOs(rrc)
+	rrc.Close()
+
+}
+
+func toValue(args ...interface{}) starlark.Tuple {
+	var tuple starlark.Tuple
+	for _, v := range args {
+		vv, err := convert.ToValue(v)
+		if err != nil {
+			panic(err)
+		}
+		tuple = append(tuple, vv)
+	}
+
+	return tuple
 }
