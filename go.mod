12ab6857e8df62948d555df1ee3fc0f54c0e71f2
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2019-09-13 08:57:48 +0200

    Bundle trees

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/bep/temp
 go 1.12
 
 require (
+	github.com/armon/go-radix v1.0.0
 	github.com/starlight-go/starlight v0.0.0-20181207205707-b06f321544f3
 	go.starlark.net v0.0.0-20190820173200-988906f77f65
 )
--- main.go
+++ main.go
@@ -1,130 +1,43 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
+	"strings"
 
-	"github.com/starlight-go/starlight"
-	"github.com/starlight-go/starlight/convert"
-	"go.starlark.net/starlark"
+	radix "github.com/armon/go-radix"
 )
 
 //
 
-type readCloserProvider interface {
-	OpenReadCloser() (io.ReadCloser, error)
-}
-
-type openReadCloser func() (io.ReadCloser, error)
-
-func (o openReadCloser) OpenReadCloser() (io.ReadCloser, error) {
-	return o()
-}
-
-type contact struct {
-	Name string
-}
-
-type myhttp struct {
-}
-
-func (myhttp) Get(s string) readCloserProvider {
-	return openReadCloser(func() (io.ReadCloser, error) {
-		resp, err := http.Get(s)
-		if err != nil {
-			return nil, err
-		}
-		return resp.Body, nil
-	})
-}
-
-// https://jsonplaceholder.typicode.com/todos
-
 func main() {
 
-	globals := map[string]interface{}{
-		"http":    myhttp{},
-		"Println": fmt.Println,
-	}
+	t := radix.New()
+	sects := radix.New()
 
-	script1 := []byte(`
-	
+	sects.Insert("/blog/sect2", "sect")
+	sects.Insert("/blog/sect2/s2_1", "sect")
 
+	t.Insert("/blog/sect2/__b_h__index.md", "_index.md")
+	t.Insert("/blog/sect2/foo/__b_h__index.md", "_index.md")
 
-url = "https://s1.demo.opensourcecms.com/wordpress/wp-json/wp/v2/posts"
+	t.Insert("/blog/sect2/foo/bar", "_index.md")
+	t.Insert("/blog/sect2/b1/__b_h_index.md", "index.md")
+	t.Insert("/blog/sect2/b1/__b_i_data.json", "data.json")
+	t.Insert("/blog/sect2/b1/__b_i_b2/sunset.jpg", "sunset.jpg")
 
-
-def getTodos():
-	return http.Get(url)
-
-`)
-
-	out, err := starlight.Eval(script1, globals, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	f1 := out["getTodos"].(*starlark.Function)
-
-	runFunc(f1)
-
+	printSection(t, "/blog/sect2/")
 }
 
-func decodeTODOs(r io.Reader) {
-	dec := json.NewDecoder(r)
-	//var todos []interface{}
-	for {
-		var m interface{}
-		if err := dec.Decode(&m); err != nil {
-			if err == io.EOF {
-				break
-			}
-			panic(err)
+func printSection(t *radix.Tree, prefix string) {
+	level := strings.Count(prefix, "/")
+	t.WalkPrefix(prefix, func(s string, v interface{}) bool {
+		currentLevel := strings.Count(s, "/")
+		if currentLevel != level {
+			return true
 		}
 
-		//todos = append(todos, m)
-		fmt.Println(m)
-	}
+		fmt.Println(s)
 
-	//fmt.Println("TODOS:", todos)
-}
-
-func runFunc(fn *starlark.Function) {
-	thread := &starlark.Thread{}
-	v, err := starlark.Call(thread, fn, nil, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	f := fromValue(v).(readCloserProvider)
-	rrc, err := f.OpenReadCloser()
-	if err != nil {
-		panic(err)
-	}
-
-	decodeTODOs(rrc)
-	rrc.Close()
-
-}
-
-func fromValue(v starlark.Value) interface{} {
-	vv := convert.FromValue(v)
-
-	return vv
-
-}
-
-func toValue(args ...interface{}) starlark.Tuple {
-	var tuple starlark.Tuple
-	for _, v := range args {
-		vv, err := convert.ToValue(v)
-		if err != nil {
-			panic(err)
-		}
-		tuple = append(tuple, vv)
-	}
-
-	return tuple
+		return false
+	})
 }
