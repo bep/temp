727e400a4216599bb44fd8303d89f6e6fbc23a68
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2019-11-12 10:04:34 +0100

    Goldmark

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.12
 require (
 	github.com/armon/go-radix v1.0.0
 	github.com/starlight-go/starlight v0.0.0-20181207205707-b06f321544f3
+	github.com/yuin/goldmark v1.1.2
 	go.starlark.net v0.0.0-20190820173200-988906f77f65
 )
 
--- main.go
+++ main.go
@@ -1,43 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"path"
-	"strings"
+	"bytes"
 
-	radix "github.com/armon/go-radix"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/parser"
 )
 
-//
-
 func main() {
-
-	t := radix.New()
-
-	t.Insert("/blog__hb_/a__hl_", "b")
-	t.Insert("/blog__hb_/b/c__hl_", "c")
-
-	getBundle := func(s string) string {
-
-		p := path.Dir(strings.TrimPrefix(s, "/blog"))
-
-		parts := strings.Split(p, "/")[1:]
-
-		for i := len(parts); i >= 0; i-- {
-			key := "/blog__hb_/" + strings.Join(parts[:i], "/") + "__hl_"
-			prefix, _, _ := t.LongestPrefix(key)
-
-			if prefix != "" {
-				return prefix
-			}
-
-		}
-
-		return ""
-
+	md := goldmark.New(
+		goldmark.WithParserOptions(
+			parser.WithAttribute(),
+		),
+	)
+	source := []byte(`
+## heading {#id .className attrName=attrValue class="class1 class2"}
+`)
+	var buf bytes.Buffer
+	if err := md.Convert(source, &buf); err != nil {
+		panic(err)
 	}
-
-	//
-
-	fmt.Println("S:", getBundle("/blog/b/c/data.json"))
 }
